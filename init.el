b19319705a3d161a6fa91868106a425096f3075f
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2026-03-15 01:29:32 +0100

    Remove duplicated code

--- init.el
+++ init.el
@@ -236,11 +236,6 @@
   :config
   (setq which-key-idle-delay 0.2))
 
-;; (use-package breadcrumb
-;;   :hook
-;;   (prog-mode . breadcrumb-local-mode)
-;;   )
-
 (use-package helpful
   :custom
   (counsel-describe-function-function #'helpful-callable)
@@ -331,10 +326,6 @@
 (custom-set-faces
  '(proof-locked-face ((t (:background "#3c3836")))))
 
-(use-package company-coq
-  :hook (coq-mode . company-coq-mode))
-
-
 (use-package treesit-auto
   :custom
   (treesit-auto-install 'prompt)
@@ -351,23 +342,8 @@
   :bind (:map why3-mode-map
 	      ("C-c C-c" . compile)))
 
-;;;;;;;;;;;;;;;;;; ocaml setup ;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(use-package direnv
-  :config
-  (direnv-mode))
-
-(use-package tuareg
-  :mode (("\\.ocamlinit\\'" . tuareg-mode))
-  :hook (tuareg-mode . display-line-numbers-mode))
-
-(use-package utop
-  :hook (tuareg-mode . utop-minor-mode))
-
-(use-package dune
-  :mode ("\\`dune\\(?:-project\\)?\\'" . dune-mode))
-
-(defun remove-highlight () (setq mouse-highlight nil))
+(defun remove-highlight ()
+  (setq mouse-highlight nil))
 
 (defun my/eglot-capf ()
   (setq-local completion-at-point-functions
@@ -430,15 +406,6 @@
 (use-package company-coq
   :hook (coq-mode . company-coq-mode))
 
-;;;;;;;;;;;;;;;;; why3 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(use-package why3
-  :defer 3
-  :load-path "lisp"
-  :mode ("\\.mlw\\'" . why3-mode)
-  :bind (:map why3-mode-map
-	      ("C-c C-c" . compile)))
-
 ;;;;;;;;;;;;;;;;;; ocaml setup ;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package direnv
@@ -450,7 +417,8 @@
   :mode (("\\.ocamlinit\\'" . tuareg-mode))
   :hook (tuareg-mode . display-line-numbers-mode))
 
-(defun remove-highlight () (setq mouse-highlight nil))
+(use-package utop
+  :hook (tuareg-mode . utop-minor-mode))
 
 (use-package opam-switch-mode
   :ensure t
